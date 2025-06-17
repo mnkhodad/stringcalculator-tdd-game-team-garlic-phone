b6521834ca51dacd46116ae60ae3c66eb4a251ae
Ananya Devpura <adevpura@andrew.cmu.edu>
2025-06-17 09:00:10 -0400

    green - return sum of two input string

--- calc.py
+++ calc.py
@@ -3,4 +3,7 @@
 def add(numbers):
     if numbers == "":
         return 0
+    if "," in numbers:
+        nums = numbers.split(",")
+        return str(int(nums[0]) + int(nums[1]))
     return numbers
